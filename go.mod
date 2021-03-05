69dc401155e25bd9a9f9ad2feb635f3e5253aaa7
Adi Saripuloh <adisaripuloh@gmail.com>
2021-03-06 01:26:17 +0700

    Add routing products

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/AdiSaripuloh/online-store
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
+
+func init() {
+	// TODO create resolver
+}
 
 func main() {
-	fmt.Println("This challenge will be solved soon!")
+	port := os.Getenv("APP_PORT")
+	router := gin.Default()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	if port == "" {
+		port = "8000"
+	}
+
+	api := router.Group("api")
+	{
+		v1 := api.Group("v1")
+		{
+			v1.GET("/products", func(ctx *gin.Context) {
+				ctx.JSON(http.StatusOK, gin.H{
+					"message": "List Products",
+				})
+			})
+		}
+	}
+
+	if err := router.Run(":" + port); err != nil {
+		log.Fatal(err)
+	}
 }
