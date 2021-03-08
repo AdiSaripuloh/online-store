4507f66b6872d332a837a409c74c3fca835d32a0
Adi Saripuloh <adisaripuloh@gmail.com>
2021-03-08 08:40:53 +0700

    Change auth logic

--- common/middlewares/auth.go
+++ common/middlewares/auth.go
@@ -1,28 +1,60 @@
 package middlewares
 
 import (
-	"github.com/AdiSaripuloh/online-store/mappers"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
 // Auth
 // Because we are not using JWT, we are use UserID in header Authorization
 // Check request has user id or not
+// Authorization: Bearer {userID}
 func Auth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		auth := ctx.Request.Header["Authorization"]
-		if len(auth) <= 0 {
-			ctx.JSON(http.StatusUnauthorized, mappers.ResponseErr("Unauthorized"))
+		authHeader := ctx.Request.Header.Get("Authorization")
+		r, _ := regexp.Compile("^Bearer (.+)$")
+
+		match := r.FindStringSubmatch(authHeader)
+
+		if len(match) == 0 {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status":  "FAILED",
+				"message": "Unauthorized",
+			})
 			ctx.Abort()
 		}
-		authSlice := strings.Split(auth[0], " ")
-		if len(authSlice) < 2 {
-			ctx.JSON(http.StatusUnauthorized, mappers.ResponseErr("Unauthorized"))
+		tokenString := match[1]
+
+		if len(tokenString) == 0 {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status":  "FAILED",
+				"message": "Unauthorized",
+			})
 			ctx.Abort()
 		}
-		userID := strings.Split(auth[0], " ")[1]
+
+		auth := strings.Split(match[0], " ")
+		if len(auth) < 2 {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status":  "FAILED",
+				"message": "Unauthorized",
+			})
+			ctx.Abort()
+		}
+		userID := auth[1]
+
+		_, err := uuid.FromString(userID)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status":  "FAILED",
+				"message": "Unauthorized",
+			})
+			ctx.Abort()
+		}
+
 		ctx.Set("UserID", userID)
 		ctx.Next()
 	}
